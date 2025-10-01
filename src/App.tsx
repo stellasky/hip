3690917eab47219c5cdc99300657fba25a6b501d
Cory Luellen <cory.luellen@gmail.com>
2025-10-01 14:00:27 -0700

    Add Authenticator and enhance Todo app functionality
    - Wrapped App component with Authenticator for user authentication.
    - Implemented sign out functionality and enabled todo item deletion on click.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
+import { useAuthenticator } from '@aws-amplify/ui-react';
 
 const client = generateClient<Schema>();
 
 function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
+  const { signOut } = useAuthenticator();
+
   useEffect(() => {
     client.models.Todo.observeQuery().subscribe({
       next: (data) => setTodos([...data.items]),
     });
   }, []);
+
+    
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id })
+  }
 
   function createTodo() {
     client.models.Todo.create({ content: window.prompt("Todo content") });
@@ -20,10 +28,13 @@ function App() {
   return (
     <main>
       <h1>My todos</h1>
+      <button onClick={signOut}>Sign out</button>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
+          <li 
+          onClick={() => deleteTodo(todo.id)}
+          key={todo.id}>{todo.content}</li>
         ))}
       </ul>
       <div>
